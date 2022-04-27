067f7dcf09792022d3278366208f9939ad54aef8
Aldo S <aldo11santiago@gmail.com>
2022-04-26 20:53:57 -0500

    added MissionCommander class and test

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const fer = new MissionCommander("Fernanda")
+console.log(fer.name)
+
+const rod = new MissionCommander("Rodrigo")
+console.log(rod.name)
